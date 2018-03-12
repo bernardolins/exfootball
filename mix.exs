a2a9c74505c3a0312466f339b0aa8e09dc698027
bernardolins <bernardo.lins@corp.globo.com>
2018-03-12 07:53:38 -0300

    Update FakeServer to 1.4.0

--- mix.exs
+++ mix.exs
@@ -12,25 +12,20 @@ defmodule Exfootball.Mixfile do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:tesla, "0.10.0"},
       {:poison, ">= 1.0.0"},
-      {:fake_server, github: "bernardolins/fake_server", branch: "feature/improve_route_response", only: :test},
+      {:fake_server, "~> 1.4", only: :test},
       {:faker, "~> 0.9", only: :test}
     ]
   end
