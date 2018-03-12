7f9d0582a8918d6739a88c687c6c3287e7981fba
bernardolins <bernardo.lins@corp.globo.com>
2018-03-12 08:04:59 -0300

    Fix random module deprecation warning

--- test/support/football_data_responses.ex
+++ test/support/football_data_responses.ex
@@ -2,9 +2,9 @@ defmodule Exfootball.Support.FootballDataResponses do
   use FakeServer.ResponseFactory
 
   def competitions_response do
-    list_size = :random.uniform(10)
+    list_size = :rand.uniform(10)
     competition_list = Enum.map(1..list_size, fn(_) ->
-      %{id: :random.uniform(1000), caption: "#{Faker.Address.country} Championship"}
+      %{id: :rand.uniform(1000), caption: "#{Faker.Address.country} Championship"}
     end)
 
     ok(competition_list, %{"content-type" => "application/json"})
